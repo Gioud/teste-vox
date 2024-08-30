36396e370b06944eac7cb7e7e4ac4eb3222d8242
Giulio <theghostnous@outlook.com>
2024-08-29 23:26:16 -0300

    concluindo desafio

--- js/main.js
+++ js/main.js
@@ -1,27 +1,23 @@
 const nome = document.querySelector("#name")
 const email = document.querySelector("#email")
 const senha = document.querySelector("#password")
-const button = document.querySelector("#form")
+const form = document.querySelector("#form")
 
-function validaSenha(senhaValor){
-    const numCaracteres = senhaValor.length
-   
-}
-
-button.addEventListener('submit', function(e) {
+form.addEventListener('submit', function(e) {
     e.preventDefault()
     const nomeValor = nome.value
     const emailValor = email.value
     const senhaValor = senha.value
 
-    if(nomeValor == '' || emailValor == '' || senhaValor == '') {
+    if(nomeValor === '' || emailValor === '' || senhaValor === '') {
         document.getElementById('mensagem').textContent = 'Todos os campos são obrigatórios.'
-        return 
+        return
     }
-   
-    if (numCaracteres < 8){
+
+    if (senhaValor.length < 8){
         document.getElementById('mensagem').textContent = 'A senha precisa de 8 caractéres'
-        return 
+        return
     }
-   
+
+    form.submit();
 })
--- php/config.php
+++ php/config.php
@@ -1,15 +1,35 @@
 <?php
 $host = 'localhost';
-$port = '3306';
-$dbname = 'desafio_vox';
-$user = 'root';
-echo 'teste';
-try {
+$port = '5432';
+$dbname = 'teste-vox';
+$user = 'giulio';
+$password = 'giulio';
+    if ($_POST) {
+        $nome = $_POST['name'];
+        $email = $_POST['email'];
+        $senha = $_POST['password'];
+        $dateTime = new DateTime();
+        $format = $dateTime->format('Y-m-d H:i:s');
 
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo 'teste';
-} catch (PDOException $e) {
-    die("Erro ao conectar com o banco de dados: " . $e->getMessage());
-}
+        try {
+
+            $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $sql = "INSERT INTO users (nome, email, senha, data_criacao) VALUES (:nome, :email, :senha, :data_criacao)";
+
+            $stmt = $pdo->prepare($sql);
+
+            $stmt->bindParam(':nome', $nome);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':senha', $senha);
+            $stmt->bindParam(':data_criacao', $format);
+
+            $stmt->execute();
+
+            echo 'Usuário cadastrado com sucesso!';
+        } catch (PDOException $e) {
+            die("Erro ao conectar com o banco de dados: " . $e->getMessage());
+        }
+    }
 ?>
